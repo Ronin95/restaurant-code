d0a6dc87cffb764ca06eb758f73bcf5280241ee3
Nikola Badjevic <Ronin95@users.noreply.github.com>
2023-04-30 17:15:32 +0200

    working on routerlink and css responsiveness

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImprintComponent } from './imprint/imprint.component';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: PathToImprintComponent},
+  { path: 'header', component: HeaderComponent},
+  { path: 'imprint', component: ImprintComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
